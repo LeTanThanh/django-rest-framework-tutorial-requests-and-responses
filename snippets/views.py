ce21662d16aa0424d18d22b7592283ee3f01de33
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-06-19 14:43:55 +0700

    Add API GET /api/v1/snippets

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.http import HttpResponseNotAllowed
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from rest_framework.parsers import JSONParser
+
+from http import HTTPStatus
+
+from .models.snippet import Snippet
+from .serializers.snippet_serializer import SnippetSerializer
+
+
+@csrf_exempt
+def snippets(request):
+    request_method = request.method
+
+    match request_method:
+        case "GET":
+            return list_snippets(request=request)
+        case "POST":
+            pass
+        case _:
+            return HttpResponseNotAllowed(permitted_methods=["GET", "POST"])
+
+
+def list_snippets(request):
+    return JsonResponse(data=[], safe=False, status=HTTPStatus.OK)
