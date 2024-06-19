e5442415bb561cc4e964550ed3fac67e515a9509
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-06-19 16:11:00 +0700

    Add API POST /api/v1/snippets

--- snippets/serializers/snippet_serializer.py
+++ snippets/serializers/snippet_serializer.py
@@ -1,9 +1,8 @@
 from rest_framework.serializers import ModelSerializer
 
 from ..models.snippet import Snippet
-from ..models.snippet import LANGUAGE_CHOICES
-from ..models.snippet import STYLE_CHOICES
 
 class SnippetSerializer(ModelSerializer):
-    model = Snippet
-    fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language']
--- snippets/views.py
+++ snippets/views.py
@@ -1,11 +1,12 @@
+import pdb
+
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.http import HttpResponseNotAllowed
 from django.views.decorators.csrf import csrf_exempt
 
+from rest_framework import status
 from rest_framework.parsers import JSONParser
-
-from http import HTTPStatus
 
 from .models.snippet import Snippet
 from .serializers.snippet_serializer import SnippetSerializer
@@ -19,10 +20,25 @@ def snippets(request):
         case "GET":
             return list_snippets(request=request)
         case "POST":
-            pass
+            return create_snippets(request=request)
         case _:
             return HttpResponseNotAllowed(permitted_methods=["GET", "POST"])
 
 
 def list_snippets(request):
-    return JsonResponse(data=[], safe=False, status=HTTPStatus.OK)
+    snippets = Snippet.objects.all()
+    serializer = SnippetSerializer(instance=snippets, many=True)
+    data = serializer.data
+
+    return JsonResponse(data=data, safe=False, status=status.HTTP_200_OK)
+
+
+def create_snippets(request):
+    data = JSONParser().parse(request)
+    serializer = SnippetSerializer(data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+
+    return JsonResponse(serializer.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
